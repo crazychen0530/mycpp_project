008fe3832a336eaa974d98583bf83d3b2a213aca
chenziyou <1052270474@qq.com>
2026-07-23 19:48:58 +0800

    add Mylogger.cc/Mylogger.hh

--- log4/Mylogger/Mylogger.cc
+++ log4/Mylogger/Mylogger.cc
@@ -1,5 +1,5 @@
 #include "Mylogger.hh"
-#include <execution>
+
 #include <iostream>
 #include <log4cpp/Category.hh>
 #include <log4cpp/Appender.hh>
@@ -7,9 +7,10 @@
 #include <log4cpp/Priority.hh>
 #include <log4cpp/OstreamAppender.hh>
 #include <log4cpp/FileAppender.hh>
+#include <string>
 
 using namespace log4cpp;
-
+Mylogger* Mylogger::_pInstance = nullptr;
 Mylogger::Mylogger(): _mycat(Category::getRoot().getInstance("mycat")){
     auto ptn1 = new PatternLayout();
     ptn1->setConversionPattern("%d %c [%p] %m%n");//d日期，c记录器名，p优先级，m消息msg，n换行符
@@ -17,4 +18,46 @@ Mylogger::Mylogger(): _mycat(Category::getRoot().getInstance("mycat")){
     ptn2->setConversionPattern("%d %c [%p] %m%n");
 
     auto pos = new OstreamAppender("console",&std::cout);
+    pos->setLayout(ptn1);//绑定布局
+
+    auto pfile  = new FileAppender("fileApp","chenziyou.log");
+    pfile->setLayout(ptn2);
+
+    _mycat.setPriority(Priority::DEBUG);
+    _mycat.addAppender(pos);
+    _mycat.addAppender(pfile);
+    std::cout<<"Mylogger()"<<std::endl;
 }
+
+Mylogger::~Mylogger(){
+    Category::shutdown();
+    std::cout<< "~Mylogger()"<<std::endl;
+}
+
+Mylogger* Mylogger::getInstance(){
+    if(nullptr == _pInstance){
+        _pInstance = new Mylogger();
+    }
+    return _pInstance;
+}
+
+void Mylogger::destory(){
+    if(_pInstance){
+        delete _pInstance;
+        _pInstance = nullptr;
+    }
+}
+
+void Mylogger::warn(const char* msg){
+    _mycat.warn(msg);
+}
+void Mylogger::error(const char* msg){
+    _mycat.error(msg);
+}
+void Mylogger::debug(const char* msg){
+    _mycat.debug(msg);
+}
+void Mylogger::info(const char* msg){
+    _mycat.info(msg);
+}
+
--- log4/Mylogger/Mylogger.hh
+++ log4/Mylogger/Mylogger.hh
@@ -3,6 +3,18 @@
 
 #include <log4cpp/Category.hh>
 
+
+
+#define addPrifix(msg) std::string("[").append(__FILE__) \
+            .append(" : ").append(__func__) \
+            .append(" : ").append(std::to_string(__LINE__)) \
+            .append(" ] ").append(msg).c_str()
+
+#define LogWarn(msg) Mylogger::getInstance()->warn(addPrefix(msg))
+#define LogInfo(msg) Mylogger::getInstance()->info(addPrefix(msg))
+#define LogDebug(msg) Mylogger::getInstance()->debug(addPrefix(msg))
+#define Logerror(msg) Mylogger::getInstance()->error(addPrefix(msg))
+
 class Mylogger
 {
 public: 
@@ -22,5 +34,5 @@ private:
     static Mylogger* _pInstance;
 };
 
-Mylogger* Mylogger::_pInstance = nullptr;
+
 #endif
